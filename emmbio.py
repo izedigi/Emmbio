ca5730fd058d3133b396d1e36dd955d8c3e2df2e
izedigi <izeigiehon@gmail.com>
2026-09-18 17:30:51 -0700

    Update print statement from 'Hello' to 'Goodbye'

--- emmbio.py
+++ emmbio.py
@@ -1417,4 +1417,19 @@ return {
 ============================================================
 22. LOCAL / RENDER STARTUP
 ============================================================
-if __name__ == "__main__": port = int(os.environ.get("PORT", 8000)) uvicorn.run("emmbio:app", host="0.0.0.0", port=port)
+if name == "main":
+port = int(
+    os.getenv(
+        "PORT",
+        "8080"
+    )
+)
+
+
+uvicorn.run(
+
+    app,
+
+    host="0.0.0.0", port=port
+)
+
