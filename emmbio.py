dd567e23125490025e8e84e6c8c09f5b799d44ca
izedigi <izeigiehon@gmail.com>
2026-09-18 18:17:04 -0700

    Update fmt.Println message from 'Hello' to 'Goodbye'

--- emmbio.py
+++ emmbio.py
@@ -1417,20 +1417,9 @@ return {
 ============================================================
 22. LOCAL / RENDER STARTUP
 ============================================================
-if name == "main":
-port = int(
-    os.getenv(
-        "PORT",
-        "8080"
-    )
-)
-
-
-uvicorn.run(
-
-    app,
-
-    host="0.0.0.0", port=port)
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", 8080))
+    uvicorn.run(app, host="0.0.0.0", port=port)
 
 
 
